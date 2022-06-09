e513f940fdd2221d7fd34891b5c999b1ecd1ec4a
Jaho <kjh13650@gmail.com>
2022-06-09 18:45:01 +0900

    hook을 사용해 title 바꾸기

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 import Header from "./components/Header";
+import useTitle from './hooks/useTitle';
 
 function App() {
+  useTitle("The Movies");
   return (
   <Router basename={process.env.PUBLIC_URL}>
     <Routes>
